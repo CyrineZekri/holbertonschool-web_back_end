b9a12b9327487b19566ce6a82ba71c2e49f836da
cyrine <cyrinezekri4@gmail.com>
2023-11-26 02:13:17 +0100

    mixed list

---
+++ python_variable_annotations/6-sum_mixed_list.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""module documentation"""
+from typing import List, Union
+
+
+def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
+    """list of different types"""
+    return sum(mxd_lst)
