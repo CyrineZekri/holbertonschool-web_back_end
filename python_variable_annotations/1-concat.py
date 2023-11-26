ac44d49eaa50594cdac0cad0951ac54c6795dfc2
cyrine <cyrinezekri4@gmail.com>
2023-11-26 01:26:28 +0100

    pycodes

--- python_variable_annotations/1-concat.py
+++ python_variable_annotations/1-concat.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 """module documentation"""
-def concat(str1:str,str2:str)->str:
+
+
+def concat(str1: str, str2: str) -> str:
     """function that concatenates two strings"""
-    return str1+str2
+    return str1 + str2
