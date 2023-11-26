856ba637971fbee37f9f8b3115f67f50250b5e66
cyrine <cyrinezekri4@gmail.com>
2023-11-26 01:37:51 +0100

    floor function

---
+++ python_variable_annotations/2-floor.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""module documentation"""
+from math import floor as math_floor
+
+
+def floor(n: float) -> int:
+    """function that calculates floor"""
+    return math_floor(n)
