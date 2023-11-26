91524c38f7d51ebf9e942722e3e8cd685e5d2942
cyrine <cyrinezekri4@gmail.com>
2023-11-26 02:21:28 +0100

    add list XD

--- python_variable_annotations/7-to_kv.py
+++ python_variable_annotations/7-to_kv.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 """module documentation"""
-from typing import Union
+from typing import Tuple, Union
 
 
-def to_kv(k: str, v: Union[int, float]) -> tuple[str, float]:
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
     """takes and returns multiple types"""
     return (k, float(v**2))
