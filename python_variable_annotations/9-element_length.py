79346a1633d05c8c713507c330d5e329e16e1a55
cyrine <cyrinezekri4@gmail.com>
2023-11-26 02:55:17 +0100

    return too is iterabe XD

--- python_variable_annotations/9-element_length.py
+++ python_variable_annotations/9-element_length.py
@@ -4,6 +4,6 @@ from typing import List, Tuple, Sequence, Iterable
 # list can take any type thus sequence
 
 
-def element_length(lst: Iterable[Sequence]) -> List[Tuple[str, int]]:
+def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
     """function documentation"""
     return [(i, len(i)) for i in lst]
