917e062fe3f9aef5566ebb7d1de6f4a1025aa752
cyrine <cyrinezekri4@gmail.com>
2023-11-26 06:10:43 +0100

    float

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -1,12 +1,11 @@
 #!/usr/bin/env python3
-"""module documentation"""
+"""documentation"""
 import asyncio
 import random
 from typing import Generator
 
-
 async def async_generator() -> Generator[float, None, None]:
-    """function definition"""
+    """documentation"""
     for _ in range(10):
         await asyncio.sleep(1)
-        yield random.randint(0, 10)
+        yield random.uniform(0, 10)
