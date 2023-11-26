012ae515eaf835b6ff6caa3e5de2f8105e03f50b
cyrine <cyrinezekri4@gmail.com>
2023-11-26 04:30:39 +0100

    calcuate

---
+++ python_async_function/2-measure_runtime.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""module documentation"""
+import asyncio
+import time
+wait_n = __import__('1-concurrent_coroutines').wait_n
+
+
+def measure_time(n: int, max_delay: int) -> float:
+    """function documentation"""
+    start_time = time.time()
+    asyncio.run(wait_n(n, max_delay))
+    end_time = time.time()
+    exec_time = end_time - start_time
+    return exec_time / n
