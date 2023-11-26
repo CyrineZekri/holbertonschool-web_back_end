92ae5005f65c6afa977de5d1092ed9ad60754103
cyrine <cyrinezekri4@gmail.com>
2023-11-26 04:37:56 +0100

    pep8

--- python_async_function/3-tasks.py
+++ python_async_function/3-tasks.py
@@ -3,7 +3,8 @@
 import asyncio
 from typing import List
 wait_random = __import__('0-basic_async_syntax').wait_random
-def task_wait_random(max_delay: int) -> asyncio.Task:
 
+
+def task_wait_random(max_delay: int) -> asyncio.Task:
     """function documentation"""
     return asyncio.create_task(wait_random(max_delay))
