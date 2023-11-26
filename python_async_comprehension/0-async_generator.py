8fe0debc3be6829d472200b41fd7fe67ecc91fc6
cyrine <cyrinezekri4@gmail.com>
2023-11-26 05:58:40 +0100

    async generator anotation

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -2,10 +2,10 @@
 """module documentation"""
 import asyncio
 import random
-from typing import Generator, NoneType
+from typing import Generator
 
 
-async def async_generator() -> AsyncGenerator[float, None]:
+async def async_generator() -> Generator[float, None,None]:
     """function definition"""
     for _ in range(10):
         await asyncio.sleep(1)
