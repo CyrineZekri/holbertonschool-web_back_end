3bed4b51dd39ffe70d0dfe1abe63af25e5b054fd
cyrine <cyrinezekri4@gmail.com>
2023-11-26 05:52:53 +0100

    async generator anotation

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -4,8 +4,9 @@ import asyncio
 import random
 from typing import AsyncGenerator
 
-async def async_generator()-> AsyncGenerator[float,None]:
+
+async def async_generator() -> AsyncGenerator[float, None]:
     """function definition"""
     for _ in range(10):
         await asyncio.sleep(1)
-        yield random.randint(0,10)
+        yield random.randint(0, 10)
