e1ecefcfe31d6a311e967369bb45fbd3d7653fd0
cyrine <cyrinezekri4@gmail.com>
2023-11-26 01:52:19 +0100

    sum list

---
+++ python_variable_annotations/5-sum_list.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""module documentation"""
+from typing import List
+
+
+def sum_list(input_list: list[float]) -> float:
+    """return sum of list of float"""
+    return sum(input_list)
