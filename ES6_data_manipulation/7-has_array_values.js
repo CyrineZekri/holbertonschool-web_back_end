7e9e69428dbbf93009cb096d9eada83f3adcdf9a
cyrine <cyrinezekri4@gmail.com>
2023-11-27 23:43:22 +0100

    verify if it has all elemnt

--- ES6_data_manipulation/7-has_array_values.js
+++ ES6_data_manipulation/7-has_array_values.js
@@ -1,10 +1,10 @@
 export default function hasValuesFromArray(set, array) {
-	for (const element of array) {
-		if (!set.has(element)) {
-			return false;
-		}
-	}
-	return true;
+  for (const element of array) {
+    if (!set.has(element)) {
+      return false;
+    }
+  }
+  return true;
 }
 
 hasValuesFromArray;
