1ceb2a453516941b6b89397be808ca8b1c53086d
cyrine <cyrinezekri4@gmail.com>
2023-11-27 17:16:41 +0100

    eslint

--- ES6_classes/5-building.js
+++ ES6_classes/5-building.js
@@ -1,22 +1,23 @@
 export default class Building {
     constructor(sqft) {
-        if (new.target !== Building && this.evacuationWarningMessage === undefined) {
-            throw new Error('Class extending Building must override evacuationWarningMessage');
-        }
-        if (typeof sqft !== 'number') {
-            throw new TypeError('sqft must be a number');
-        }
-        this._sqft = sqft;
+      if (new.target !== Building && this.evacuationWarningMessage === undefined) {
+        throw new Error('Class extending Building must override evacuationWarningMessage');
+      }
+      if (typeof sqft !== 'number') {
+        throw new TypeError('sqft must be a number');
+      }
+      this._sqft = sqft;
     }
-
+  
     get sqft() {
-        return this._sqft;
+      return this._sqft;
     }
-
+  
     set sqft(value) {
-        if (typeof value !== 'number') {
-            throw new TypeError('sqft must be a number');
-        }
-        this._sqft = value;
+      if (typeof value !== 'number') {
+        throw new TypeError('sqft must be a number');
+      }
+      this._sqft = value;
     }
-}
+  }
+  
