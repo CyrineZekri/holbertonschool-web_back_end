df6f17fbcc82654870b6501b1642a32d042b12f5
cyrine <cyrinezekri4@gmail.com>
2023-11-27 17:14:53 +0100

    input

--- ES6_classes/6-sky_high.js
+++ ES6_classes/6-sky_high.js
@@ -1,23 +1,20 @@
-import Building from './5-building.js';
+import Building from './5-building';
 
 export default class SkyHighBuilding extends Building {
-    constructor(sqft, floors) {
-        super(sqft);
-        this._floors = floors;
-    }
+  constructor(sqft, floors) {
+    super(sqft);
+    this._floors = floors;
+  }
 
-    get floors() {
-        return this._floors;
-    }
+  get floors() {
+    return this._floors;
+  }
 
-    set floors(value) {
-        if (typeof value !== 'number') {
-            throw new TypeError('floors must be a number');
-        }
-        this._floors = value;
-    }
+  set floors(value) {
+    this._floors = value;
+  }
 
-    evacuationWarningMessage() {
-        return `Evacuate slowly the ${this._floors} floors.`;
-    }
+  evacuationWarningMessage() {
+    return `Evacuate slowly the ${this.floors} floors`;
+  }
 }
