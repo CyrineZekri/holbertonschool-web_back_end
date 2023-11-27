4d6c3403a9feeeae2a056affaf213c2e51d0c614
cyrine <cyrinezekri4@gmail.com>
2023-11-28 00:44:56 +0100

    update fil

--- ES6_data_manipulation/4-update_grade_by_city.js
+++ ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,12 +1,12 @@
 export default function updateStudentGradeByCity(students, city, newGrades) {
-	return students
-		.filter((student) => student.location === city)
-		.map((student) => {
-			const matchingGrade = newGrades.find((gradeObj) => gradeObj.studentId === student.id);
-			return {
-				...student,
-				grade: matchingGrade ? matchingGrade.grade : 'N/A',
-			};
-		});
+  return students
+    .filter((student) => student.location === city)
+    .map((student) => {
+      const matchingGrade = newGrades.find((gradeObj) => gradeObj.studentId === student.id);
+      return {
+        ...student,
+        grade: matchingGrade ? matchingGrade.grade : 'N/A',
+      };
+    });
 }
 
