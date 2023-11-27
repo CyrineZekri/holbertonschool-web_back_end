cd18d294bca8f2ca1b930b710ab4a0b850c91081
cyrine <cyrinezekri4@gmail.com>
2023-11-27 02:04:34 +0100

    eslint

--- ES6_classes/3-currency.js
+++ ES6_classes/3-currency.js
@@ -1,9 +1,9 @@
 export default class Currency{
   constructor(code,name){
-    if(typeof code!=='string') throw TypeError('Code must be a string');
-    if(typeof name!=='string') throw TypeError('Code must be a string');
-    this._code=code;
-    this._name=name
+    if(typeof code !== 'string') throw TypeError('Code must be a string');
+    if(typeof name !== 'string') throw TypeError('Code must be a string');
+    this._code = code;
+    this._name = name
   }
   get name(){
     return this._name;
@@ -12,11 +12,11 @@ export default class Currency{
     return this._code;
   }
   set name(value){
-    if(typeof value!=='string') throw TypeError('Code must be a string');
+    if(typeof value !== 'string') throw TypeError('Code must be a string');
     this._name=value;
   }    set code(value){
-    if(typeof value!=='string') throw TypeError('Code must be a string');
-    this._code=value;
+    if(typeof value !== 'string') throw TypeError('Code must be a string');
+    this._code = value;
   }
   displayFullCurrency() {
     return `${this._name} (${this._code})`;
