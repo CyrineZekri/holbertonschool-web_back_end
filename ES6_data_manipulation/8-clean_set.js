745da4c9df395d7acbc14894cfe5334e871b4d2e
cyrine <cyrinezekri4@gmail.com>
2023-11-27 23:51:01 +0100

    checks on set

---
+++ ES6_data_manipulation/8-clean_set.js
@@ -0,0 +1,14 @@
+export default function cleanSet(set, startString) {
+	const cleanedValues = [];
+
+	if (!set instanceof Set || typeof startString !== 'string' || startString.length == 0) {
+		return '';
+	}
+
+	for (const item of set) {
+		if (item && item.startsWith(startString)) {
+			cleanedValues.push(item.slice(startString.length));
+		}
+	}
+	return cleanedValues.join('-');
+}
