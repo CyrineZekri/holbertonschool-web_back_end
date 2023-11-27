3ce37d2b8fc68b570c61d5e0103fb66e7c54e836
cyrine <cyrinezekri4@gmail.com>
2023-11-27 23:52:38 +0100

    eslint

--- ES6_data_manipulation/8-clean_set.js
+++ ES6_data_manipulation/8-clean_set.js
@@ -1,14 +1,14 @@
 export default function cleanSet(set, startString) {
-	const cleanedValues = [];
+  const cleanedValues = [];
 
-	if (!set instanceof Set || typeof startString !== 'string' || startString.length == 0) {
-		return '';
-	}
+  if (!set instanceof Set || typeof startString !== 'string' || startString.length == 0) {
+    return '';
+  }
 
-	for (const item of set) {
-		if (item && item.startsWith(startString)) {
-			cleanedValues.push(item.slice(startString.length));
-		}
-	}
-	return cleanedValues.join('-');
+  for (const item of set) {
+    if (item && item.startsWith(startString)) {
+      cleanedValues.push(item.slice(startString.length));
+    }
+  }
+  return cleanedValues.join('-');
 }
