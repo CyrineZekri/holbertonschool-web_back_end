42fc6a8e1bc5b5ffa8f33756bab516d601ca2ba0
cyrine <cyrinezekri4@gmail.com>
2024-02-19 18:08:57 +0100

    fifo caching

--- caching/1-fifo_cache.py
+++ caching/1-fifo_cache.py
@@ -2,10 +2,11 @@
 """BaseCaching module"""
 BaseCaching = __import__('base_caching').BaseCaching
 
+
 class FIFOCache(BaseCaching):
     """FIFO cache Algorithms
     """
-    
+
     def __init__(self):
         """Initialize"""
         super().__init__()
@@ -20,7 +21,7 @@ class FIFOCache(BaseCaching):
             self.queue.append(key)
         if len(self.cache_data) > BaseCaching.MAX_ITEMS:
             first_in = self.queue.pop(0)
-            if first_in in self.cache_data:  
+            if first_in in self.cache_data:
                 del self.cache_data[first_in]
                 print("DISCARD: {}".format(first_in))
 
